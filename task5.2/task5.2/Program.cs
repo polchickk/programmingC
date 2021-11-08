4a5b9f6fec3a7c1459f53e265354a6469b9a48d3
polina <p.l.pavlova@urfu.me>
2021-11-08 17:47:55 +0500

    задание 5.2

---
+++ task5.2/task5.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "чемпионат";
+            string result = str.Substring(3, 1)
+                + str.Substring(5, 2)
+                + str.Substring(0, 1)
+                + str.Substring(5, 1);
+            string res = str.Substring(8, 1)
+                + str.Substring(1, 3);
+            Console.WriteLine(result);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
