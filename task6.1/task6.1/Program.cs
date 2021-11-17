7d4b226b6e6738abc43587eb3aae30291e46fa1b
polina <p.l.pavlova@urfu.me>
2021-11-17 18:30:39 +0500

    задание 6.1 выполнено

---
+++ task6.1/task6.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = Input("m");
+            var n = Input("n");
+            Console.WriteLine(IsMultiplyAndSum(m, n));
+            Console.ReadKey();
+        }
+        static int Input(string name)
+        {
+            Console.WriteLine($"Введите целое число {name}");
+            return int.Parse(Console.ReadLine());
+        }
+        static bool multiply(int m, int n)
+        {
+            return (m * n) % 5 == 0;
+        }
+        static bool sum(int m, int n)
+        {
+            return (m + n) % 5 != 0;
+        }
+        static bool IsMultiplyAndSum(int m, int n)
+        {
+            var a = multiply(m, n);
+            var b = sum(m, n);
+            return a && b;
+        
+         }
+    }
+}
