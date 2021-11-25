926b943b51e6d6017a537705072f583067f85419
polina <p.l.pavlova@urfu.me>
2021-11-25 14:51:01 +0500

    исправлено 6.1

--- task6.1/task6.1/Program.cs
+++ task6.1/task6.1/Program.cs
@@ -12,28 +12,35 @@ namespace task6._1
         {
             var m = Input("m");
             var n = Input("n");
+           
             Console.WriteLine(IsMultiplyAndSum(m, n));
+            
             Console.ReadKey();
         }
+        
         static int Input(string name)
         {
             Console.WriteLine($"Введите целое число {name}");
             return int.Parse(Console.ReadLine());
         }
+       
         static bool multiply(int m, int n)
         {
             return (m * n) % 5 == 0;
         }
+        
         static bool sum(int m, int n)
         {
             return (m + n) % 5 != 0;
         }
+       
         static bool IsMultiplyAndSum(int m, int n)
         {
             var a = multiply(m, n);
             var b = sum(m, n);
+            
             return a && b;
-        
+
          }
     }
 }
