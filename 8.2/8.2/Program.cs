be895a594c5a8af063fdd544206ea4e17bdfb04f
polina <p.l.pavlova@urfu.me>
2021-11-25 18:23:15 +0500

    сделано 2

---
+++ 8.2/8.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество вводимых значений");
+            var n = int.Parse(Console.ReadLine());
+
+            var proiz = 1.0;
+
+            for (var i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Введите положительное действительное число");
+                var a = int.Parse(Console.ReadLine());
+                
+                var product = Math.Pow(a * proiz, 1.0 / i);
+                proiz *= a;
+                
+                Console.WriteLine($"Среднее геометрическое: {product}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
