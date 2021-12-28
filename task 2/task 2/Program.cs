7eb913e957b75871ffed17a0d79fbc70a057fb58
polina <p.l.pavlova@urfu.me>
2021-12-29 01:42:22 +0500

    сделано задание 2

--- task 2/task 2/Program.cs
+++ task 2/task 2/Program.cs
@@ -10,12 +10,14 @@ namespace task_2
     {
         static void Main(string[] args)
         {
-            int number1 = 21;
-            int number2 = -3;
+            Console.WriteLine($"Введите 2 натуральных числа ");
+            var number1 = int.Parse(Console.ReadLine());
+            var number2 = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"Произведение: {number1} * {number2} = {Multiply(number1,number2)}");
 
-           //Console.WriteLine($"Произведение: {number1} * {number2} = {Divide(number1, number2)}");
+          Console.WriteLine($"Частное: {number1} / {number2} = {Divide(number1, number2)}");
+           Console.WriteLine( Remainder(number1, number2));
 
             Console.ReadKey();
         }
@@ -29,16 +31,33 @@ namespace task_2
             return result;
         }
 
-        //static int Divide(int number1, int number2)
-        //{
-        //    int result = 0;
-            
-        //    for (int i = 0; i < number1; i--)
-        //    {
-        //        result += number2;
-        //    }
+        static int Divide(int number1, int number2)
+        {
+            int count = 0;
 
-        //    return result;
-        //}
+                while (number1 >= number2)
+                {
+                    number1 -= number2;
+                    count += 1;
+                }
+
+            return count;
+        }
+
+        static int Remainder(int number1, int number2)
+        {
+            if (number1 < number2)
+                return 0;
+            int count = 0;
+           
+            while (number1 >= number2)
+            {
+                number1 -= number2;
+                count += 1;
+            }
+            
+            return number1;
+
+        }
     }
 }
