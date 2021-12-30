ee61323b860ca50e4a32d3c2a929ac13857af236
polina <p.l.pavlova@urfu.me>
2021-12-30 12:58:42 +0500

    задание 8

---
+++ task8exam/task8exam/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsCubeNumber(number));
+
+
+            Console.ReadKey();
+        }
+        static bool IsCubeNumber(int number)
+        {
+            for (var i = 1; i <= number; i++)
+            {
+                if (number == i * i * i)
+                    return true;
+                
+            }
+            return false;
+        }
+    }
+}
