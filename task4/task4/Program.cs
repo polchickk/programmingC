34d5469b0856adeb04b95b64286ea35e67f3f3f7
polina <p.l.pavlova@urfu.me>
2021-12-30 13:52:32 +0500

    задание 4 не до конца

---
+++ task4/task4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 0;
+            var p = 0;
+            for (var i = 2; i <= 1000; i++)
+            {
+                number = i;
+                if (number % 2 != 0)
+                {
+                    for (var m = 1; m < number; m++)
+                    {
+                        if (TheSieveOfEratosthenes(p) && (number == p + 2 * m * m))
+                            break;
+                        else
+                            Console.WriteLine(number);
+                    }
+                }
+            }
+        }
+       
+        static bool TheSieveOfEratosthenes(int number)
+        {
+            
+            for (var i = 2; i*i <= number; i++)
+              
+
+        }
+    }
+}
