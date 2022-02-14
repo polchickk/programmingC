1edbc690d378c9816f17d40a4e8e5b9da2a33732
polina <p.l.pavlova@urfu.me>
2022-02-14 18:47:36 +0500

    mhhhgvhx

---
+++ EuclidGeometry2D/EuclidGeometry2D/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EuclidGeometry2D
+{
+    public class Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x,double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ EuclidGeometry2D/GeometryConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EuclidGeometry2D;
+
+namespace GeometryConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            var point = new Point(1, 2);
+        }
+
+        static void PrintPoint(Point p)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
