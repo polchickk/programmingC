116f3a4a9ca5dba4bdd8314c691aca77afe9f76a
polina <p.l.pavlova@urfu.me>
2022-02-21 17:18:28 +0500

    преобразование типов

--- WebService/WebService/Program.cs
+++ WebService/WebService/Program.cs
@@ -23,6 +23,16 @@ namespace WebService
 
             var users = new[] { george, kate, john,paul };
 
+            object obj = new object();
+            //downcast user->registereduser
+            //equals сравнение(тип ссылка и тип значение), hash code(словарь), gettype(возвращает тип объекта),tostring(переводит лююой объект в строку,чтобы выводить на консоль)
+            //безопасное приведение типов: var newObject = oldObject as NewType;
+            //клюючевое IS NewType newObject; if(oldObject is NewType) newObject = (NewType)oldObject; else {...//do smth}/// if(ildObject is NewType) {var newObject =(NewType)oldObject;...}-if(oldObject is NewType newObject){..}
+
+            Class1 regUser = paul as Class1;
+
+            VipUser vip = kate as VipUser;
+
             foreach (var user in users)
                 user.PrintInfo();
 
