2cf196bc4da2949eadb3db2dd51cfad9b66fc63a
polina <p.l.pavlova@urfu.me>
2022-02-28 18:37:36 +0500

    одшди

---
+++ VectorAlgebra/VectorAlgebra/Vector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VectorAlgebra
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Vector operator +(Vector a,Vector b)
+        {
+            return new Vector(
+                a.X + b.X,
+                a.Y + b.Y,
+                a.Z + b.Z);    
+        }
+
+        public override string ToString()
+        {
+            return $"({X}; {Y}; {Z})";
+        }
+    }
+}
---
+++ VectorAlgebra/Vectors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorAlgebra;
+
+
+namespace Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(-2, 4, 0);
+
+            Console.WriteLine(a+b);
+            Console.ReadKey();
+        }
+    }
+}
