df3ec103ad7c8e520ccad36e8eb8ac1be815dcb7
polina <p.l.pavlova@urfu.me>
2022-02-28 17:37:21 +0500

    млп

---
+++ shapes/shapes/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+    public class Rectangle : Shapes
+
+    {
+        public Point TopLeft;
+
+        private float width;
+
+        public float Height
+        {
+            get { return width; }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Отрицательная ширина");
+
+                width = value;
+            }
+        }
+        private float height;
+
+        public float Height
+        {
+            get { return height; }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Отрицательная высота");
+
+                width = value;
+            }
+        }
+        public override void  Draw()
+        {
+            Console.Write("Рисуем ");
+            PrintInfo();
+        }
+        public override void PtintInfo()
+        {
+            Console.WriteLine($"Прямоугольнрик с вершиной ({TopLeft.X};{TopLeft.Y}) шириной {Width} и высотой {Height}");
+        }
+
+    }
+}
---
+++ shapes/shapes/Shapes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace shapes
+{
+    public abstract class Shapes
+    {
+        public abstract Color OutlineColor { get; set; }
+        public abstract Color FillColor { get; set; }
+        public abstract void Draw();
+        public abstract void PrintInfo();
+
+    }
+}
