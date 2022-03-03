aeb221335c7ba5f18c1695070cbb6eb46e35b215
polina <p.l.pavlova@urfu.me>
2022-03-03 21:22:23 +0500

    атвптвчпт

---
+++ ClassLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Class1();
+            Console.WriteLine(x);
+            Console.ReadKey();
+            
+        }
+    }
+}
