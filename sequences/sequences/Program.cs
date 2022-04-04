63d0d23c034b64d9ce2c703edba584f8c73c6e3f
polina <p.l.pavlova@urfu.me>
2022-04-04 18:40:36 +0500

    последовательности

---
+++ sequences/sequences/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Numerics;
+
+namespace sequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new Sequence();
+
+            Console.WriteLine("Последовательность Фибоначчи");
+            PrintSequence(sequence.Fibonacci());
+
+            Console.WriteLine("\nФакториалы");
+            PrintSequence(sequence.Factorial());
+
+            Console.ReadKey();
+        }
+
+        static void PrintSequence(IEnumerable<BigInteger> s)
+        {
+            var i = 1;
+            foreach(var elem in s)
+            {
+
+                Thread.Sleep(500); //выводит результат через 500мс
+                Console.WriteLine($"{i++} = {elem}");
+
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+
+                    if (key.Key == ConsoleKey.Enter || key.Key == ConsoleKey.Escape)
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ sequences/sequences/Sequence.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace sequences
+{
+    public class Sequence
+    {
+        public IEnumerable<BigInteger> Fibonacci()
+        {
+            BigInteger a = 1;
+            BigInteger b = 1;
+            BigInteger c;
+
+            yield return a;
+            yield return b;
+
+            while (true)
+            {
+                c = a + b;
+               yield return c;
+                (a, b) = (b, c);
+            }
+        }
+
+        public IEnumerable<BigInteger> Factorial()
+        {
+            BigInteger n = 1;
+            BigInteger f = 1;
+
+            while (true)
+            {
+                yield return f;
+                f *= n++;
+            }
+        }
+    }
+}
