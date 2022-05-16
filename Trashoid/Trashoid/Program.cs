8c5a0d13b394e09305d78e7282f62d681a6bd717
polina <p.l.pavlova@urfu.me>
2022-05-16 17:36:41 +0500

    queue

---
+++ Trashoid/Trashoid/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trashold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел");
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new int[n];
+            var rnd = new Random();
+
+            for (var i = 0; i < n; i++)
+                numbers[i] = rnd.Next(1000);
+
+            PrintSequence(numbers);
+
+            Console.WriteLine("Введите значение порога");
+            var trashold = int.Parse(Console.ReadLine());
+
+            var small = new Queue<int>();
+            var big = new Queue<int>();
+
+            foreach (var number in numbers)
+                if (number < trashold)
+                    small.Enqueue(number);
+                else
+                    big.Enqueue(number);
+
+            PrintSequence(small);
+            PrintSequence(big);
+
+            Console.ReadKey();
+        }
+
+        static void PrintSequence(IEnumerable<int> sequence)
+        {
+            foreach(var elem in sequence)
+                Console.Write($"{elem} ");
+
+            Console.WriteLine("\n");
+        }
+    }
+}
