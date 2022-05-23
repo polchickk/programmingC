e522d1e9e1e79de850acddd7b9aca9fa36d125bf
polina <p.l.pavlova@urfu.me>
2022-05-23 18:56:21 +0500

    элементы управления

--- Controls/Controls/MainForm.Designer.cs
+++ Controls/Controls/MainForm.Designer.cs
@@ -29,20 +29,285 @@ namespace Controls
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.captionLabel = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.inputButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.loadRTFbutton = new System.Windows.Forms.Button();
+            this.boldCheckBox = new System.Windows.Forms.CheckBox();
+            this.italicCheckBox = new System.Windows.Forms.CheckBox();
+            this.undelineCheckBox = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.stringListBox = new System.Windows.Forms.ListBox();
+            this.fontComboBox = new System.Windows.Forms.ComboBox();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.myProgressBar = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // captionLabel
+            // 
+            this.captionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.captionLabel.Location = new System.Drawing.Point(12, 23);
+            this.captionLabel.Name = "captionLabel";
+            this.captionLabel.Size = new System.Drawing.Size(485, 31);
+            this.captionLabel.TabIndex = 0;
+            this.captionLabel.Text = "Надпись";
+            this.captionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.Location = new System.Drawing.Point(17, 103);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(471, 26);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputLabel.Location = new System.Drawing.Point(17, 67);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(136, 20);
+            this.inputLabel.TabIndex = 2;
+            this.inputLabel.Text = "Введите текст";
+            // 
+            // inputButton
+            // 
+            this.inputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputButton.Location = new System.Drawing.Point(21, 160);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(104, 28);
+            this.inputButton.TabIndex = 3;
+            this.inputButton.Text = "Ввод";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Visible = false;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(520, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(465, 392);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // loadRTFbutton
+            // 
+            this.loadRTFbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadRTFbutton.Location = new System.Drawing.Point(520, 437);
+            this.loadRTFbutton.Name = "loadRTFbutton";
+            this.loadRTFbutton.Size = new System.Drawing.Size(215, 39);
+            this.loadRTFbutton.TabIndex = 5;
+            this.loadRTFbutton.Text = "Загрузить текст";
+            this.loadRTFbutton.UseVisualStyleBackColor = true;
+            this.loadRTFbutton.Click += new System.EventHandler(this.loadRTFbutton_Click);
+            // 
+            // boldCheckBox
+            // 
+            this.boldCheckBox.AutoSize = true;
+            this.boldCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.boldCheckBox.Location = new System.Drawing.Point(21, 215);
+            this.boldCheckBox.Name = "boldCheckBox";
+            this.boldCheckBox.Size = new System.Drawing.Size(133, 24);
+            this.boldCheckBox.TabIndex = 6;
+            this.boldCheckBox.Text = "полужирный";
+            this.boldCheckBox.UseVisualStyleBackColor = true;
+            this.boldCheckBox.CheckedChanged += new System.EventHandler(this.ChangeFontStyle);
+            // 
+            // italicCheckBox
+            // 
+            this.italicCheckBox.AutoSize = true;
+            this.italicCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.italicCheckBox.Location = new System.Drawing.Point(21, 255);
+            this.italicCheckBox.Name = "italicCheckBox";
+            this.italicCheckBox.Size = new System.Drawing.Size(87, 24);
+            this.italicCheckBox.TabIndex = 6;
+            this.italicCheckBox.Text = "курсив";
+            this.italicCheckBox.UseVisualStyleBackColor = true;
+            this.italicCheckBox.CheckedChanged += new System.EventHandler(this.ChangeFontStyle);
+            // 
+            // undelineCheckBox
+            // 
+            this.undelineCheckBox.AutoSize = true;
+            this.undelineCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.undelineCheckBox.Location = new System.Drawing.Point(21, 295);
+            this.undelineCheckBox.Name = "undelineCheckBox";
+            this.undelineCheckBox.Size = new System.Drawing.Size(152, 24);
+            this.undelineCheckBox.TabIndex = 6;
+            this.undelineCheckBox.Text = "подчеркнутый";
+            this.undelineCheckBox.UseVisualStyleBackColor = true;
+            this.undelineCheckBox.CheckedChanged += new System.EventHandler(this.ChangeFontStyle);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(260, 215);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 24);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.Text = "10 пт";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.ChangeFontSize);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(260, 255);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 24);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "14 пт";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.ChangeFontSize);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(260, 295);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 24);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.Text = "18 пт";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.ChangeFontSize);
+            // 
+            // stringListBox
+            // 
+            this.stringListBox.FormattingEnabled = true;
+            this.stringListBox.ItemHeight = 16;
+            this.stringListBox.Location = new System.Drawing.Point(12, 331);
+            this.stringListBox.Name = "stringListBox";
+            this.stringListBox.Size = new System.Drawing.Size(476, 84);
+            this.stringListBox.Sorted = true;
+            this.stringListBox.TabIndex = 8;
+            this.stringListBox.SelectedIndexChanged += new System.EventHandler(this.stringListBox_SelectedIndexChanged);
+            // 
+            // fontComboBox
+            // 
+            this.fontComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.fontComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fontComboBox.FormattingEnabled = true;
+            this.fontComboBox.Location = new System.Drawing.Point(260, 160);
+            this.fontComboBox.Name = "fontComboBox";
+            this.fontComboBox.Size = new System.Drawing.Size(228, 28);
+            this.fontComboBox.TabIndex = 9;
+            this.fontComboBox.SelectedIndexChanged += new System.EventHandler(this.fontComboBox_SelectedIndexChanged);
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.PictureBox.Location = new System.Drawing.Point(12, 421);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(476, 235);
+            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox.TabIndex = 10;
+            this.PictureBox.TabStop = false;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 662);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 32);
+            this.backButton.TabIndex = 11;
+            this.backButton.Text = "<<";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.ChangePicture);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(413, 662);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(75, 32);
+            this.forwardButton.TabIndex = 12;
+            this.forwardButton.Text = ">>";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.ChangePicture);
+            // 
+            // myProgressBar
+            // 
+            this.myProgressBar.Location = new System.Drawing.Point(12, 715);
+            this.myProgressBar.Maximum = 150;
+            this.myProgressBar.Name = "myProgressBar";
+            this.myProgressBar.Size = new System.Drawing.Size(476, 23);
+            this.myProgressBar.Step = 25;
+            this.myProgressBar.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(997, 807);
+            this.Controls.Add(this.myProgressBar);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.fontComboBox);
+            this.Controls.Add(this.stringListBox);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.undelineCheckBox);
+            this.Controls.Add(this.italicCheckBox);
+            this.Controls.Add(this.boldCheckBox);
+            this.Controls.Add(this.loadRTFbutton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.inputButton);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.captionLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Элементы управления";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label captionLabel;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button loadRTFbutton;
+        private System.Windows.Forms.CheckBox boldCheckBox;
+        private System.Windows.Forms.CheckBox italicCheckBox;
+        private System.Windows.Forms.CheckBox undelineCheckBox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ListBox stringListBox;
+        private System.Windows.Forms.ComboBox fontComboBox;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.ProgressBar myProgressBar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog textOpenFileDialog;
     }
 }
-
--- Controls/Controls/MainForm.cs
+++ Controls/Controls/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,150 @@ namespace Controls
 {
     public partial class MainForm : Form
     {
+        int pictureNumber;
         public MainForm()
         {
             InitializeComponent();
+
+            stringListBox.Items.Add("Москва");
+            stringListBox.Items.Add("Екатеринбург");
+            stringListBox.Items.Add("Санкт-Петербург");
+            stringListBox.Items.Add("Владивосток");
+
+            fontComboBox.Items.Add("Microsoft San Serif");
+            fontComboBox.Items.Add("Times New Roman");
+            fontComboBox.Items.Add("Courier New");
+            fontComboBox.Items.Add("Arial");
+            fontComboBox.SelectedIndex = 0;
+
+            pictureNumber = 7;
+            LoadPicture();
+
+            textOpenFileDialog.Filter = "Простой текст|*.txt|Форматированный текст|*.rtf";
+            textOpenFileDialog.InitialDirectory = Application.StartupPath;
+
+
+        }
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0)
+            {
+                inputButton.Visible = true;
+                inputLabel.Visible = false;
+            }
+
+            else
+            {
+                inputButton.Visible = false;
+                inputLabel.Visible = true;
+            }
+
+        }
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            captionLabel.Text = inputTextBox.Text;
+            inputTextBox.Text = "";
+        }
+
+        private void ChangeFontStyle(object sender, EventArgs e)
+        {
+            var fontStyle = FontStyle.Regular;
+
+            if (boldCheckBox.Checked)
+                fontStyle |= FontStyle.Bold;
+
+            if (italicCheckBox.Checked)
+                fontStyle |= FontStyle.Italic;
+
+            if (undelineCheckBox.Checked)
+                fontStyle |= FontStyle.Underline;
+
+            captionLabel.Font = new Font(
+                captionLabel.Font.FontFamily,
+                captionLabel.Font.Size,
+                fontStyle);
+        }
+
+        private void ChangeFontSize(object sender, EventArgs e)
+        {
+            var radiobutton = sender as RadioButton;
+            int size = int.Parse(radiobutton.Text.Split()[0]);
+
+            captionLabel.Font = new Font(
+                captionLabel.Font.FontFamily,
+                size,
+                captionLabel.Font.Style);
+        }
+
+        private void fontComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var fontFamilyName = fontComboBox.SelectedItem.ToString();
+
+            captionLabel.Font = new Font(
+                fontFamilyName,
+                captionLabel.Font.Size,
+                captionLabel.Font.Style);
+        }
+
+        private void stringListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            captionLabel.Text = stringListBox.SelectedItem.ToString();
+        }
+        private void ChangePicture(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            myProgressBar.Step = 25;
+
+            if (button.Text == "<<")
+                pictureNumber--;
+            else
+                pictureNumber++;
+            if (pictureNumber == 8)
+                pictureNumber = 1;
+            else if (pictureNumber == 0)
+                pictureNumber = 7;
+
+            LoadPicture();
+            myProgressBar.Value = (pictureNumber - 1) * myProgressBar.Step;
+        }
+
+        private void LoadPicture()
+        {
+            var pic = Application.StartupPath + @"\Images\pic" + pictureNumber.ToString() + ".jpg";
+            PictureBox.Load(pic);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            myProgressBar.PerformStep();
+
+            if (myProgressBar.Value % 1000 == 0)
+            {
+                pictureNumber++;
+                LoadPicture();
+            }
+
+            if (pictureNumber == 7)
+                timer1.Enabled = false;
+        }
+        private void slideshowButton_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            pictureNumber = 1;
+            myProgressBar.Value = 0;
+            myProgressBar.Step = 1;
+            LoadPicture();
+        }
+
+        private void loadRTFbutton_Click(object sender, EventArgs e)
+        {
+
+            if (textOpenFileDialog.ShowDialog() == DialogResult.OK)
+                if (textOpenFileDialog.FilterIndex == 1)
+                    richTextBox1.Text = File.ReadAllText(textOpenFileDialog.FileName);
+                else
+                    richTextBox1.LoadFile(textOpenFileDialog.FileName);
         }
     }
 }
