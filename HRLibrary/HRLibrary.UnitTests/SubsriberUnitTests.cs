21c1794a5ec8102953db01dd7ac8603bba586ed5
polina <p.l.pavlova@urfu.me>
2022-06-20 17:07:03 +0400

    тесты

---
+++ HRLibrary/HRLibrary.UnitTests/SubsriberUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using HRLibrary;
+
+
+namespace HRLibrary.UnitTests
+{
+    [TestFixture]
+    public class SubsriberUnitTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var max = CreateTestSubsriber();
+
+            Assert.AreEqual("Max", max.Name);
+            Assert.AreEqual("Ramen", max.Surname);
+            Assert.AreEqual("891234567890",max.TelephoneNumber);
+            Assert.AreEqual(258496, max.Number);
+            //Assert.AreEqual("Best", max.NameOfTheTariff);
+            //Assert.AreEqual(PaymentType.Credit, max.Payment);
+            //Assert.AreEqual(8964.56, max.Sum);
+
+        }
+
+        [Test]
+        public void ToString_Subsriber_NameSpaceSurname()
+        {
+            var max = CreateTestSubsriber();
+            Assert.AreEqual("Max Ramen", max.ToString());
+        }
+        private Subsriber CreateTestSubsriber()
+        {
+            return new Subsriber("Max", "Ramen", "891234567890", 258496 );
+        }
+    }
+}
---
+++ HRLibrary/HRLibrary/Subsriber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class Subsriber
+    {
+        public string Name;
+        public string Surname;
+        public string TelephoneNumber;
+        public readonly int Number;
+        public string NameOfTheTariff;
+        public double Sum;
+        public PaymentType Payment;
+
+        public Subsriber(string name, string surname, string telephonenumber, int number)
+        {
+            Name = name;
+            Surname = surname;
+            TelephoneNumber = telephonenumber;
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}";
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(this);
+
+            var payment = "";
+            switch (Payment)
+            {
+                case PaymentType.Credit:
+                    payment = "кредит";
+                    break;
+                case PaymentType.Prepayment:
+                    payment = "предоплата";
+                    break;
+            }
+
+            Console.WriteLine($"Телефонный номер: {TelephoneNumber}. Номер договора: {Number}. Название тарифа: {NameOfTheTariff}. Тип оплаты: {payment}. Сумма на личном счете: {Sum}.");
+        }
+    }
+}
